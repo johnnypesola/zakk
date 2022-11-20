1349512880f774b891da624c241adb027fbb7fbb
Johnny Pesola <johnny@pesola.eu>
2022-11-20 22:02:58 +0100

    Make possible to render amazing zakk gfx

--- src/main.js
+++ src/main.js
@@ -13,40 +13,120 @@ const G =[ undefined, undefined, undefined, undefined, undefined, undefined, und
 const H =[ undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined];
 const zakkboard = [A, B, C, D, E, F, G, H];
 
-const main = () => {
-drawZakkBoard();
+const zakkPieces = [
+  "pawn",
+  "rook",
+  "knight",
+  "bishop",
+  "queen",
+  "king",
+];
+
+const playerColors = ["black", "white"];
+
+const zakkPieceImages = [];
+
+const main = async () => {
+  await loadImages();
+  drawZakkBoard();
+  initZakkPieces();
+}
+
+const initZakkPieces = () => {
+  playerColors.forEach((playerColor) => {
+    const y = playerColor === "white" ? 0 : zakkboard.length - 1;
+    
+    // Render pawns
+    const pawnY = playerColor === "white" ? y + 1 : y - 1;
+    for(let i = 0; i < A.length; i++){
+      renderZakkPiece(playerColor, "pawn", i, pawnY);
+    }
+    renderZakkPiece(playerColor, "rook", 0, y);
+    renderZakkPiece(playerColor, "knight", 1, y);
+    renderZakkPiece(playerColor, "bishop", 2, y);
+    renderZakkPiece(playerColor, "queen", 3, y);
+    renderZakkPiece(playerColor, "king", 4, y);
+    renderZakkPiece(playerColor, "bishop", 5, y);
+    renderZakkPiece(playerColor, "knight", 6, y);
+    renderZakkPiece(playerColor, "rook", 7, y);
+  })
+}
+
+const renderZakkPiece = (playerColor, zakkPieceName, x, y) => {
+  drawBoardSquare(x, y);
+  const imageToDraw = zakkPieceImages.find((image) => image.src.includes(playerColor + "-" + zakkPieceName));
+  ctx.drawImage(imageToDraw, x * squareSize, y * squareSize);
+}
+
+const loadImage = async (imgName) => {
+  var resolveFn;
+
+  var promise = new Promise((resolve) => {
+    resolveFn = resolve;
+  });
+
+  const img = new Image();   // Create new img element
+
+  zakkPieceImages.push(img);
+
+  img.addEventListener('load', () => {
+    resolveFn();
+  });
+
+  img.src = "img/" + imgName + ".png"; // Set source path
+
+  return promise;
+}
+
+const loadImages = async () => {
+  const promises = [];
+
+  playerColors.forEach(async (playerColor) => {
+    zakkPieces.forEach(async (piece) => {
+      const imageLoadingPromise = loadImage(playerColor + "-" + piece)
+      promises.push(imageLoadingPromise);
+    })
+  })
+  
+  await Promise.all(promises);
 }
 
 const drawZakkBoard = () => {
   
   for (var rowNum = 0; rowNum < zakkboard.length; rowNum++) {
    const currentRow = zakkboard[rowNum];
-   Row(currentRow.length, rowNum)
+   drawBoardRow(currentRow.length, rowNum)
  }
 }
 
-function Row(numberOfSquaresToDraw, rowNum) {
-    for (let colNum = 0; colNum < numberOfSquaresToDraw; colNum++) {
-      const xPosition = squareSize*colNum
-      const yPostition = squareSize*rowNum;
-      const isOddCol = colNum%2;
-      const isOddRow = rowNum%2
-      const isOdd = (isOddCol) - (isOddRow);
+const isOddSquare = (x, y) => {
+  const isOddCol = x%2;
+  const isOddRow = y%2
+  const isOdd = (isOddCol) - (isOddRow);
+  return isOdd;
+}
 
-      drawPostitionSquare(xPosition, yPostition,isOdd);
+const drawBoardRow = (numberOfSquaresToDraw, rowNum) => {
+    for (let colNum = 0; colNum < numberOfSquaresToDraw; colNum++) {
+      drawBoardSquare(colNum, rowNum);
     }
-  }
-function drawPostitionSquare(x, y, isFilled) {
+}
+
+const drawBoardSquare = (x, y) => {
+  const xPos = squareSize*x
+  const yPos = squareSize*y;
+  const isFilled = isOddSquare(x, y)
+
   if (isFilled) {
-    ctx.fillRect(x, y, squareSize, squareSize);
+    ctx.fillStyle = "#aaa";
+    ctx.fillRect(xPos, yPos, squareSize, squareSize);
     ctx.closePath();
 
   } else { 
-    ctx.rect(x, y, squareSize, squareSize);
-    ctx.stroke();
+    ctx.fillStyle = "#888";
+    ctx.fillRect(xPos, yPos, squareSize, squareSize);
     ctx.closePath();
   }
 }
-
 
 main();
